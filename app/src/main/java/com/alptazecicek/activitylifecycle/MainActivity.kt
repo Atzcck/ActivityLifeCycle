0690ba45b1c8465c004c71fd783aefc0382b48ed
Atzcck <alptazecicek@gmail.com>
2022-09-05 15:21:13 +0300

    Activity Lifecycle added to MainActivity.kt

--- app/src/main/java/com/alptazecicek/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/alptazecicek/activitylifecycle/MainActivity.kt
@@ -2,10 +2,42 @@ package com.alptazecicek.activitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.v("systemOut","onCreate called.")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v("systemOut","onStart called.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v("systemOut","onResume called.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v("systemOut","onPause called.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v("systemOut","onStop called.")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v("systemOut", "onRestart called.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v("systemOut","onDestroy called.")
     }
 }
